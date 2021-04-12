4b9df0d7264f914630a28ba7e85659eb01110172
inlym <inlym@qq.com>
2021-04-12 17:19:22 +0800

    :bookmark: v1.2.1 版本发布

--- app/middleware/tracer.js
+++ app/middleware/tracer.js
@@ -4,7 +4,7 @@ module.exports = (options, app) => {
   return async function tracer(ctx, next) {
     const { idHeader } = options
     if (!ctx.response.get(idHeader)) {
-      ctx.set(idHeader) = ctx.traceId
+      ctx.set(idHeader, ctx.traceId)
     }
     await next()
   }
